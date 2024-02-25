8ea95cfe77ddbcd9fdddb193f78554688279b8f7
Orry Verducci <orry@orryverducci.co.uk>
2024-02-25 12:48:05 +0000

    Change Docker build output folder

--- docker/build-scripts/build.sh
+++ docker/build-scripts/build.sh
@@ -6,7 +6,7 @@ apt-get update
 
 # Publish the app without the app host, which is not required by the runtime Docker image
 
-dotnet publish "/build/src/StreamSDR.csproj" /p:UseAppHost=false /p:PublishSingleFile=false -c Release -o /build/publish
+dotnet publish "/build/src/StreamSDR.csproj" /p:UseAppHost=false /p:PublishSingleFile=false -c Release -o /build/app
 
 # Clean up package list
 
