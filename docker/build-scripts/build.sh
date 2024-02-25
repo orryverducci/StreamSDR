d26aff33df4e4d00fdafcd47eed6211f4cf8de2f
Orry Verducci <orry@orryverducci.co.uk>
2024-02-25 12:46:45 +0000

    Use absolute path

--- docker/build-scripts/build.sh
+++ docker/build-scripts/build.sh
@@ -6,7 +6,7 @@ apt-get update
 
 # Publish the app without the app host, which is not required by the runtime Docker image
 
-dotnet publish "src/StreamSDR.csproj" /p:UseAppHost=false /p:PublishSingleFile=false -c Release -o /build/publish
+dotnet publish "/build/src/StreamSDR.csproj" /p:UseAppHost=false /p:PublishSingleFile=false -c Release -o /build/publish
 
 # Clean up package list
 
