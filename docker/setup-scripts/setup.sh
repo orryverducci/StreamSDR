f7ae268f88e233c0a1904efdc27299b2dbb5f61b
Orry Verducci <orry@orryverducci.co.uk>
2025-12-13 17:56:04 +0000

    Move user setup into a separate script

--- docker/setup-scripts/setup.sh
+++ docker/setup-scripts/setup.sh
@@ -14,9 +14,7 @@ source /tmp/scripts/install-dependencies-sdrplay.sh
 
 # Create a non-root user with an explicit UID and add permission to access the /app folder
 
-apt-get install -y --no-install-recommends adduser
-adduser -u 5678 --disabled-password --gecos "" appuser
-chown -R appuser /app
+source /tmp/scripts/setup-user.sh
 
 # Clean up package list
 
